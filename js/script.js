3dfe68bdfca423a129c840b0964b5b3c35249d4f
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2023-01-03 00:51:12 +0000

    Hide quotes when no advice

--- js/script.js
+++ js/script.js
@@ -3,8 +3,10 @@
 const button = document.querySelector('.iconBackground');
 const adviceNumber = document.querySelector('.adviceNumber');
 const adviceMessage = document.querySelector('.advice');
+const adviceField = document.querySelector('.adviceField');
 
 const insertAdvice = (id, advice) => {
+    adviceField.classList.add('active');
     adviceNumber.innerHTML = id;
     adviceMessage.innerHTML = advice;
 }
